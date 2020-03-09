d807964a8f04c3bce2b0ec53d5c42fc65dc14032
Matthew Munson <mmunson11@gmail.com>
2020-03-09 01:39:53 -0700

    Implemented TileMap!

--- public_html/src/MyGame/MyGame.js
+++ public_html/src/MyGame/MyGame.js
@@ -15,28 +15,34 @@ function MyGame() {
     // The camera to view the scene
     this.mCamera = null;
 
-    this.mMsg = null;
 
-    this.mLineSet = [];
-    this.mCurrentLine = null;
-    this.mP1 = null;
+    this.tileMap = new TileMap(-20, -20, 3, 10, 10);
+    
+    
 }
 gEngine.Core.inheritPrototype(MyGame, Scene);
 
 MyGame.prototype.initialize = function () {
     // Step A: set up the cameras
     this.mCamera = new Camera(
-        vec2.fromValues(30, 27.5), // position of the camera
+        vec2.fromValues(0, 0), // position of the camera
         100,                       // width of camera
         [0, 0, 640, 480]           // viewport (orgX, orgY, width, height)
     );
     this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
             // sets the background to gray
 
-    this.mMsg = new FontRenderable("Status Message");
-    this.mMsg.setColor([0, 0, 0, 1]);
-    this.mMsg.getXform().setPosition(-19, -8);
-    this.mMsg.setTextHeight(3);
+
+    for(var i = 0; i < 10; i++)
+    {
+        for(var j = 0; j < 10; j++)
+        {
+            var test = new Renderable();
+            test.setColor([i/10, j/10, i/10, 1]);
+            
+            this.tileMap.addTile(i,j, test);
+        }
+    }
 };
 
 // This is the draw function, make sure to setup proper drawing environment, and more
@@ -46,50 +52,13 @@ MyGame.prototype.draw = function () {
     gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
 
     this.mCamera.setupViewProjection();
-    var i, l;
-    for (i = 0; i < this.mLineSet.length; i++) {
-        l = this.mLineSet[i];
-        l.draw(this.mCamera);
-    }
-    this.mMsg.draw(this.mCamera);   // only draw status in the main camera
+
+    this.tileMap.draw(this.mCamera);
 };
 
 // The Update function, updates the application state. Make sure to _NOT_ draw
 // anything from this function!
-MyGame.prototype.update = function () {
-    var msg = "Lines: " + this.mLineSet.length + " ";
-    var echo = "";
-    var x, y;
-
-    if (gEngine.Input.isButtonPressed(gEngine.Input.mouseButton.Middle)) {
-        var len = this.mLineSet.length;
-        if (len > 0) {
-            this.mCurrentLine = this.mLineSet[len - 1];
-            x = this.mCamera.mouseWCX();
-            y = this.mCamera.mouseWCY();
-            echo += "Selected " + len + " ";
-            echo += "[" + x.toPrecision(2) + " " + y.toPrecision(2) + "]";
-            this.mCurrentLine.setFirstVertex(x, y);
-        }
-    }
-
-    if (gEngine.Input.isButtonPressed(gEngine.Input.mouseButton.Left)) {
-        x = this.mCamera.mouseWCX();
-        y = this.mCamera.mouseWCY();
-        echo += "[" + x.toPrecision(2) + " " + y.toPrecision(2) + "]";
-
-        if (this.mCurrentLine === null) { // start a new one
-            this.mCurrentLine = new LineRenderable();
-            this.mCurrentLine.setFirstVertex(x, y);
-            this.mLineSet.push(this.mCurrentLine);
-        } else {
-            this.mCurrentLine.setSecondVertex(x, y);
-        }
-    } else {
-        this.mCurrentLine = null;
-        this.mP1 = null;
-    }
-
-    msg += echo;
-    this.mMsg.setText(msg);
+MyGame.prototype.update = function ()
+{
+   
 };
--- public_html/src/MyGame/TileMap.js
+++ public_html/src/MyGame/TileMap.js
@@ -5,3 +5,70 @@
  */
 
 
+function TileMap(xPos, yPos, tileWidth, width, height)
+{
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.tileWidth = tileWidth;
+    
+    this.width = width;
+    this.height = height;
+    
+    this.tiles = [];
+    
+    
+    this.initialize();
+    
+}
+
+
+TileMap.prototype.initialize = function () 
+{
+    for(var i = 0; i < this.width; i++)
+    {
+        var col = [];
+        
+        for(var j = 0; j < this.height; j++)
+        {
+            col[j] = 0;
+        }
+        
+        this.tiles[i] = col;
+    }
+ 
+};
+
+TileMap.prototype.addTile = function(tileX, tileY, textureRenderable)
+{
+    textureRenderable.getXform().setXPos(tileX * this.tileWidth + this.xPos);
+    textureRenderable.getXform().setYPos(tileY * this.tileWidth + this.yPos);
+    
+    textureRenderable.getXform().setSize(this.tileWidth, this.tileWidth);
+    
+    this.tiles[tileX][tileY] = textureRenderable;  
+    
+};
+
+
+
+TileMap.prototype.draw = function (camera)
+{
+    for(var i = 0; i < this.width; i++)
+    {        
+        for(var j = 0; j < this.height; j++)
+        {
+            if(this.tiles[i][j] !== 0)
+            {
+               this.tiles[i][j].draw(camera);
+            }
+        }
+    }
+
+    
+};
+
+TileMap.prototype.update = function ()
+{
+    
+    
+};
